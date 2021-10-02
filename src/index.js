3621420ea59c8fd972823ffc15c697e7ee4defde
Ahmed <ahmdadl563@gmail.com>
2021-10-02 02:57:52 +0200

    Just some comments needed to be deleted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
-// import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
-// // eslint-disable-next-line no-use-before-define
-// import '../node_modules/@fortawesome/fontawesome-free/js/all';
-
 import './style.css';
 import './localStorage';
