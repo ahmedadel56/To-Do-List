9931ff9c5c895eacf2b6d69df939f206666083b9
Ahmed <ahmdadl563@gmail.com>
2021-10-02 02:56:35 +0200

    Everything added and tested

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
-// eslint-disable-next-line no-use-before-define
-import '../node_modules/@fortawesome/fontawesome-free/js/all';
+// import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
+// // eslint-disable-next-line no-use-before-define
+// import '../node_modules/@fortawesome/fontawesome-free/js/all';
 
 import './style.css';
 import './localStorage';
